af44b2aec9e0e7d6de7c88cfe720a3fe725e2957
dzamosen <dzamosen@redhat.com>
2023-09-29 13:33:28 +0200

    Exeecise #3

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import "fmt"
 func main () {
 // #1 hello world 
 	fmt.Println("Hello World")
+	fmt.Println()
 
 
 // #2 Values
@@ -16,4 +17,25 @@ func main () {
 	fmt.Println(true && false)
 	fmt.Println(true || false)
 	fmt.Println(!true)
+	fmt.Println()
+
+// #3 Variables
+
+	var first = "string first"
+	fmt.Println(first)
+
+	var a, b int = 3, 56
+	fmt.Println(a, b)
+
+	var lie = true
+	fmt.Println(lie)
+
+	var z int
+	fmt.Println(z)
+
+	new := "new variable"
+	fmt.Println(new)
+
+	fmt.Println()
+
 }
