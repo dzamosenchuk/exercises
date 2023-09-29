16e805a5c5e240655cc29cd9190b4425afd420c7
dzamosen <dzamosen@redhat.com>
2023-09-29 13:24:10 +0200

    Exercise #2

--- main.go
+++ main.go
@@ -9,11 +9,11 @@ func main () {
 
 // #2 Values
 
-fmt.Println("go" + "lang")
-fmt.Println("2+2=", 1+1)
-fmt.Println("5.0/8.0=", 5.0/8.0)
+	fmt.Println("go" + "lang")
+	fmt.Println("2+2=", 1+1)
+	fmt.Println("5.0/8.0=", 5.0/8.0)
 
-fmt.Println(true && false)
-fmt.Println(true || false)
-fmt.Println(!true)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
 }
