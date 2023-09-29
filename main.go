fc1b2aba379374585ecea0603db2e9cbbee5d4c1
dzamosen <dzamosen@redhat.com>
2023-09-29 13:22:57 +0200

    Exercise #2

--- main.go
+++ main.go
@@ -6,4 +6,14 @@ func main () {
 // #1 hello world 
 	fmt.Println("Hello World")
 
+
+// #2 Values
+
+fmt.Println("go" + "lang")
+fmt.Println("2+2=", 1+1)
+fmt.Println("5.0/8.0=", 5.0/8.0)
+
+fmt.Println(true && false)
+fmt.Println(true || false)
+fmt.Println(!true)
 }
