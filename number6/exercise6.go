8cf53b8ed9bc55009b4b8a1af41872d4637eb263
dzamosen <dzamosen@redhat.com>
2023-09-29 14:42:15 +0200

    Exercise 6

---
+++ number6/exercise6.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i, "is even")
+		} else {
+			fmt.Println(i, "is odd")
+		}
+	}
+	fmt.Println()
+
+	if 8%4 == 0 {
+		fmt.Println("8 is divisible by 4")
+	}
+
+	if num := 9; num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
+	}
+}
