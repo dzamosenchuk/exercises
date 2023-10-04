a96c5bd40cc9f8ad9e6610db025af8775dc586b7
dzamosen <dzamosen@redhat.com>
2023-10-04 09:38:34 +0200

    Third version of exersise

--- go_cat/cat.go
+++ go_cat/cat.go
@@ -1,30 +1,77 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
 )
 
-func check(e error) {
+func check(e error) {     //check errors
     if e != nil {
      //   panic(e)
-	 fmt.Println("Error ", e)
+	 fmt.Println("Error is ", e)
 	 os.Exit(1)
     }
 }
+func countLines(fileName string) {    //count all lines in file
+	file, err := os.Open(fileName) 
+	check(err)
+	countLines :=0
+	fileScanner := bufio.NewScanner(file)
+		for fileScanner.Scan(){
+			countLines++
+		}
+	fmt.Println("Number of lines is ", countLines)
+}
+
+func countLinesB(fileName string) {   //count not empty lines in file
+	file, err := os.Open(fileName) 
+	check(err)
+	countLines :=0
+	fileScanner := bufio.NewScanner(file)
+		for fileScanner.Scan(){
+		//if   != "" {
+			countLines++
+		//}
+			
+		}
+	fmt.Println("Number of lines is ", countLines)
+}
+func fileRead(fileName string) {    //read and print file
+	dat, err := os.ReadFile(fileName)
+    check(err)
+    fmt.Print(string(dat))
+}
+
 func main() {
-	pathfile := flag.String("f", " ", "f")
-	
+	pathfile := flag.String("f", "", "f")
+	countStrings := flag.String("n", "", "n")
+	countStringsB := flag.String("b", "", "b")
+
 	flag.Parse()
+	
 	if *pathfile == " " {
 		fmt.Println("Use -f for define file name and path")
 		os.Exit(1)
+	} else {
+		fileRead(*pathfile)
 	}
-	fmt.Println("Name of file is ", *pathfile)
 
-	dat, err := os.ReadFile(*pathfile)
-    check(err)
-    fmt.Print(string(dat))
+	countLines(*countStrings)
+
+	countLinesB(*countStringsB)
+
+	//
+	//	fmt.Println("Use -f for define file name and path")
+	//	os.Exit(1)
+	//}
+
+
+	
+
+	
+
+	
 
 }
