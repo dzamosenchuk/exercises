4a5c3095e6418fdb49056e47207635f59e84687c
dzamosen <dzamosen@redhat.com>
2023-10-04 15:08:31 +0200

    small fixes in formatting

--- go_cat/cat.go
+++ go_cat/cat.go
@@ -7,41 +7,41 @@ import (
 	"os"
 )
 
-func check(e error) {     //check errors
-    if e != nil {
-     //   panic(e)
-	 fmt.Println("Error is ", e)
-	 os.Exit(1)
-    }
+func check(e error) { //check errors
+	if e != nil {
+		//   panic(e)
+		fmt.Println("Error is ", e)
+		os.Exit(1)
+	}
 }
-func countLines(fileName string) {    //count all lines in file
-	file, err := os.Open(fileName) 
+func countLines(fileName string) { //count all lines in file
+	file, err := os.Open(fileName)
 	check(err)
-	countLines :=0
+	countLines := 0
 	fileScanner := bufio.NewScanner(file)
-		for fileScanner.Scan(){
-			countLines++
-		}
+	for fileScanner.Scan() {
+		countLines++
+	}
 	fmt.Println("Number of lines is ", countLines)
 }
 
-func countLinesB(fileName string) {   //count not empty lines in file
-	file, err := os.Open(fileName) 
+func countLinesB(fileName string) { //count not empty lines in file
+	file, err := os.Open(fileName)
 	check(err)
-	countLines :=0
+	countLines := 0
 	fileScanner := bufio.NewScanner(file)
-		for fileScanner.Scan(){
-		if  fileScanner.Text() != "" {
+	for fileScanner.Scan() {
+		if fileScanner.Text() != "" {
 			countLines++
 		}
-			
-		}
+
+	}
 	fmt.Println("Number of lines is ", countLines)
 }
-func fileRead(fileName string) {    //read and print file
+func fileRead(fileName string) { //read and print file
 	dat, err := os.ReadFile(fileName)
-    check(err)
-    fmt.Print(string(dat))
+	check(err)
+	fmt.Print(string(dat))
 }
 
 func main() {
@@ -50,20 +50,20 @@ func main() {
 	countStringsB := flag.String("b", "", "b")
 
 	flag.Parse()
-	
+
 	switch {
-	case  len(*pathfile) > 0 :
+	case len(*pathfile) > 0:
 		fileRead(*pathfile)
-	
-	case len(*countStrings) > 0 :
+
+	case len(*countStrings) > 0:
 		countLines(*countStrings)
 
-	case len(*countStringsB) > 0 :
+	case len(*countStringsB) > 0:
 		countLinesB(*countStringsB)
-	
+
 	default:
-    	fmt.Println("Any flags are defined. Use -f for print a file, -n for count strings in a file, -b for count all non empty strings.")
-		os.Exit(1)	
+		fmt.Println("Any flags are defined. Use -f for print a file, -n for count strings in a file, -b for count all non empty strings.")
+		os.Exit(1)
 	}
 
 }
