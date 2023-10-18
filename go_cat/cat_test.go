0002adabda29100f252b3f178215f3eac9fc660d
dzamosen <dzamosen@redhat.com>
2023-10-18 14:05:22 +0200

    next one

--- go_cat/cat_test.go
+++ go_cat/cat_test.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"testing"
 )
+
 // Testing fuction of counting lines in a file
 func TestCountLines (t *testing.T) {
 	//Arange
 	nameFile := "1_test.txt"
+	d1 := []byte("hello\ngo\n")
+    f, err := os.Create(nameFile)
+    err := os.WriteFile(nameFile, d1, 0644)
+    
+	if err != nil {
+		fmt.Println("Error is ", err)
+		os.Exit(1)
+	}
+	
 	expected := 9
-
+	defer f.Close()
 	//Act
 	result := countLines(nameFile)
 	
